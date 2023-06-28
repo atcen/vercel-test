6c4cd36101a548c7f90a6f47e07c036ac4b7f486
Jimie Dean Garrett <jimiedeangarrett@gmail.com>
2023-06-28 22:19:44 +0200

    refactor: removed obsolete route

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,6 +1,20 @@
-import { redirect } from '@sveltejs/kit';
+import { sleep } from '$lib/utils';
+
+let cold = true;
 
 /** @type {import('./$types').PageServerLoad} */
-export function load() {
-	throw redirect(308, '/node')
+export function load(event) {
+	const was_cold = cold;
+	cold = false;
+
+	const ip = event.getClientAddress();
+	const city = decodeURIComponent(event.request.headers.get('x-vercel-ip-city') ?? 'unknown');
+
+	return {
+		streamed: {
+			details: sleep({ ip, city }, 1000)
+		},
+		now: new Date().toISOString(),
+		cold: was_cold
+	};
 }
